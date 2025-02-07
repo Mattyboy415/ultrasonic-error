044f2b80ffdb85cdcd90b8b12bf00c5e12d4c5a4
Mattyboy415 <mahern@tivertonschools.org>
2025-02-07 09:31:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let sonar = 0
+basic.showNumber(5)
+cuteBot.forward()
+basic.forever(function () {
+    sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (sonar < 20 && sonar > 2) {
+        cuteBot.motors(0, -50)
+        basic.pause(randint(100, 200))
+    } else {
+        cuteBot.forward()
+    }
+})
